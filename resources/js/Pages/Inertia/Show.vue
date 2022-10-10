9d0ce496cbd2224107cf2cdf202ee64ff9dca0d7
ryosuke1202 <ryosuke12021@gmail.com>
2022-10-10 16:22:54 +0900

    詳細表示

--- resources/js/Pages/Inertia/Show.vue
+++ resources/js/Pages/Inertia/Show.vue
@@ -1,14 +1,24 @@
 <script setup>
-    import { Link } from '@inertiajs/inertia-vue3';
-    defineProps({
-        id: String
+import { Link } from '@inertiajs/inertia-vue3';
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+    id: String,
+    blog: Object
+})
+
+const deleteBlog = (id) => {
+    Inertia.delete(`/inertia/delete/${id}`, {
+        onBefore: () => confirm('削除してもよろしいでしょうか？')
     })
+}
 </script>
     
 <template>
     <div>
-        Inertia/Showです<br>
-        {{ id }}
+        <p>件名: {{ blog.title }}</p>
+        <p>本文: {{ blog.content }}</p>
+        <button @click="deleteBlog(blog.id)">削除</button>
     </div>
     
 </template>
