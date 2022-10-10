442591647ffcb810326e69d8f53f5d8bd0242d15
ryosuke1202 <ryosuke12021@gmail.com>
2022-10-10 16:22:43 +0900

    一覧表示

--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -1,14 +1,21 @@
 <script setup>
-    import { Link } from '@inertiajs/inertia-vue3';
+import { Link } from '@inertiajs/inertia-vue3'
+defineProps({
+    blogs: Array
+})
 </script>
     
 <template>
     <div>
-        Inertia/Indexです<br>
-        <a href="/">aタグ経由です</a><br>
-        <Link href="/">Link経由です</Link><br>
+        <div v-if="$page.props.flash.message" class="bg-blue-300">{{ $page.props.flash.message }}</div>
+
+        <ul v-for="blog in blogs" :key="blog.id">
+            <li>
+                <Link :href="route('inertia.show', { id: blog.id })" class="text-blue-300">件名 : {{ blog.title }}</Link>
+                本文 : {{ blog.content }}
+            </li>
+        </ul>
     </div>
-    
 </template>
 
 
