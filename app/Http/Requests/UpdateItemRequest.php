9785c11cc42248755e987dea71883573e8dd7f0c
ryosuke1202 <ryosuke12021@gmail.com>
2022-10-11 23:21:13 +0900

    ItemRequestの追加

--- app/Http/Requests/UpdateItemRequest.php
+++ app/Http/Requests/UpdateItemRequest.php
@@ -13,7 +13,7 @@ class UpdateItemRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class UpdateItemRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'max:20'],
+            'memo' => ['nullable', 'max:256'],
+            'price' => ['required', 'integer'],
         ];
     }
 }
