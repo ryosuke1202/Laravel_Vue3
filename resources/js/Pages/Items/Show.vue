24085c42a6296923f8289854ecebeb2bd6c8c1b1
ryosuke1202 <ryosuke12021@gmail.com>
2022-10-11 23:22:11 +0900

    CRUDコンポーネントの追加

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,12 +1,18 @@
+<script setup>
+
+defineProps({
+    item: Object
+})
+</script>
+
 <template>
-$END$
+    <div>
+        {{ item.name }}
+        {{ item.memo }}
+        {{ item.price }}
+    </div>
 </template>
 
-<script>
-export default {
-name: "Show"
-}
-</script>
 
 <style scoped>
 
