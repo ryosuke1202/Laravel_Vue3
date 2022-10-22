5fea5a6507532d3575f81dd6dec438b8fc5fd3b2
ryosuke1202 <ryosuke12021@gmail.com>
2022-10-22 16:01:50 +0900

    ItemのCRUD処理

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,82 +5,107 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ItemController extends Controller
 {
+    public function __construct(Item $item)
+    {
+        $this->item = $item;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        $items = $this->item->select('id', 'name', 'memo', 'price', 'is_selling')->get();
+
+        return Inertia::render('Items/Index', ['items' => $items]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreItemRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreItemRequest $request
+     * @return RedirectResponse
      */
-    public function store(StoreItemRequest $request)
+    public function store(StoreItemRequest $request): RedirectResponse
     {
-        //
+        $this->item->create($request->all());
+
+        return to_route('items.index')->with([
+            'message' => '登録しました',
+            'status' => 'success'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @param Item $item
+     * @return Response
      */
-    public function show(Item $item)
+    public function show(Item $item): Response
     {
-        //
+        return Inertia::render('Items/Show', ['item' => $item]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @param Item $item
+     * @return \Response
      */
-    public function edit(Item $item)
+    public function edit(Item $item): Response
     {
-        //
+        return Inertia::render('Items/Edit', ['item' => $item]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \App\Http\Requests\UpdateItemRequest  $request
-     * @param  \App\Models\Item  $item
+     * @param Item $item
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
+        $item->update($request->all());
+
+        return to_route('items.index')->with([
+            'message' => '更新しました',
+            'status' => 'success'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
+     * @param Item $item
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy(Item $item)
     {
-        //
+        $item->delete();
+
+        return back()->with([
+            'message' => '削除しました',
+            'status' => 'danger'
+        ]);
     }
 }
