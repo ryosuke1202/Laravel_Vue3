4a9d222fa40beacd4bed47766596b5c73a877a2d
ryosuke1202 <ryosuke12021@gmail.com>
2022-11-06 01:48:02 +0900

    fillable追加

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,18 @@ class Customer extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'kana',
+        'tel',
+        'email',
+        'postcode',
+        'address',
+        'birthday',
+        'gender',
+        'emmemoail'
+    ];
+
     public function scopeSearchCustomers($query, $input = null)
     {
         if (!empty($input)) {
