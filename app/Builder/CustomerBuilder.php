b6642dc8165239fd8543dbb8d42611c723efeb69
ryosuke1202 <ryosuke12021@gmail.com>
2022-11-20 01:44:06 +0900

    カスタムクエリビルだを追加

---
+++ app/Builder/CustomerBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Builder;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class CustomerBuilder extends Builder
+{
+    /**
+     * 顧客検索
+     *
+     * @param string|null $input
+     * @return static
+     */
+    public function searchCustomers(?string $input): static
+    {
+        return $this->where('kana', 'like', '%' . $input . '%' )
+                    ->orWhere('tel', 'like', '%' . $input . '%');
+    }
+    
+}
