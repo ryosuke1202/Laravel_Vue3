c1e6cc44a20c97ec1a9feef233b2dfa793924a4c
ryosuke1202 <ryosuke12021@gmail.com>
2022-11-26 23:18:43 +0900

    分析api追加

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AnalysisController;
 use App\Http\Controllers\Api\SearchCustomer;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('searchCustomers', SearchCustomer::class);
+    Route::get('searchCustomers', SearchCustomer::class)->name('api.searchCustomers');
+    Route::get('analysis', AnalysisController::class)->name('api.analysis');
 });
