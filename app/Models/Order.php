4bd818933917fae3c1de639b9f79f8e56d7deaa3
ryosuke1202 <ryosuke12021@gmail.com>
2022-12-03 14:03:00 +0900

    rnf分析

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -210,4 +210,82 @@ class Order extends Model
     
             return [$data, $labels, $totals];
     }
+
+    public function getRmf()
+    {
+        // RMF分析
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::query()
+            ->betweenDate($startDate, $endDate)
+            ->groupBy('id')
+            ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase, created_at');
+
+        // datediffで日付の差分, maxで日付の最新日
+        // 2. 会員毎にまとめて最終購入日、回数、合計金額を 取得
+        $subQuery = DB::table($subQuery)
+            ->groupBy('customer_id')
+            ->selectRaw('
+                customer_id, customer_name, max(created_at) as recentDate,
+                datediff(now(), max(created_at)) as recency, count(customer_id) as frequency,
+                sum(totalPerPurchase) as monetary'
+            );
+
+        // 4. 会員毎のRFMランクを計算
+        $subQuery = DB::table($subQuery)
+            ->selectRaw('customer_id, customer_name, recentDate, recency, frequency, monetary, 
+                case
+                    when recency < 14 then 5
+                    when recency < 28 then 4
+                    when recency < 60 then 3
+                    when recency < 90 then 2
+                    else 1 end as r,
+                case
+                    when 7 <= frequency then 5
+                    when 5 <= frequency then 4
+                    when 3 <= frequency then 3
+                    when 2 <= frequency then 2
+                    else 1 end as f,
+                case
+                    when 300000 <= monetary then 5
+                    when 200000 <= monetary then 4
+                    when 100000 <= monetary then 3
+                    when 30000 <= monetary then 2
+                    else 1 end as m'
+            );
+
+        // 5.ランク毎の数を計算する
+        $rCount = DB::table($subQuery)
+            ->groupBy('r')
+            ->selectRaw('r, count(r)')
+            ->orderBy('r', 'desc')
+            ->get();
+        $fCount = DB::table($subQuery)
+            ->groupBy('f')
+            ->selectRaw('f, count(f)')
+            ->orderBy('f', 'desc')
+            ->get();
+        $mCount = DB::table($subQuery)
+            ->groupBy('m')
+            ->selectRaw('m, count(m)')
+            ->orderBy('m', 'desc')
+            ->get();
+
+        // concatで文字列結合
+        // 6. RとFで2次元で表示してみる 
+        $data = DB::table($subQuery)
+            ->groupBy('r')
+            ->selectRaw('
+                concat("r_", r) as rRank,
+                count(case when f = 5 then 1 end ) as f_5,
+                count(case when f = 4 then 1 end ) as f_4,
+                count(case when f = 3 then 1 end ) as f_3,
+                count(case when f = 2 then 1 end ) as f_2,
+                count(case when f = 1 then 1 end ) as f_1
+            ')
+            ->orderBy('rRank', 'desc')
+            ->get();
+
+        return $data;
+
+    }
 }
