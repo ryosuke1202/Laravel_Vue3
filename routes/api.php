bb7e40bbb02f8bb74876cb920a38f0133b578eb1
ryosuke1202 <ryosuke12021@gmail.com>
2022-12-07 01:15:43 +0900

    test

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Api\AnalysisController;
 use App\Http\Controllers\Api\SearchCustomer;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;   
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('searchCustomers', SearchCustomer::class)->name('api.searchCustomers');
